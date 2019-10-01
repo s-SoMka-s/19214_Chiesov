4750e14e05dc5fbf03387ea4db082c2c8d193bf4
s-SoMka-s <S.O.M.K.A@yandex.ru>
2019-10-01 18:16:10 +0700

    border checks added

--- qEquationSolver/qSolver.hs
+++ qEquationSolver/qSolver.hs
@@ -1,6 +1,9 @@
+solver 0 0 0 = error "Infinity Number of Solutions"
+solver 0 0 _ = error "No Solutions"
 solver a b c
-    | a == 0.0 = (x, x)
-    | a /= 0.0 = if d<0 then error "No Solutions" else (x1, x2)
+    | a == 0 = (x, x)
+    | d == 0 = (x, x)
+    | a /= 0 = if d<0 then error "No Solutions" else (x1, x2)
         where
             x = -c / b
             x1 = (-b + sqrt d ) / (2*a)
