2cee5542c17918edfdb5490aac99d34902165ee3
s-SoMka-s <S.O.M.K.A@yandex.ru>
2019-10-14 16:25:25 +0700

    added some list-funcs

---
+++ lab1-list/lists.hs
@@ -0,0 +1,24 @@
+myHead :: [a] -> a
+myHead [] = error "Empty list"
+myHead (x:xs) = x
+
+myLast :: [a] -> a
+myLast [] = error "Empty list"
+myLast [x] = x
+myLast (x:xs) = myLast xs
+
+
+myTail :: [a] -> [a]
+myTail [] = error "Empty list"
+myTail [x] = []
+myTail (x:xs) = xs
+
+myInit :: [a]->[a]
+myInit [] = error "Empty list"
+myInit [x] = []
+myInit (x:xs) = x : myInit xs
+
+myReverse :: [a] -> [a]
+myReverse [] = error "Empty list"
+myReverse [x] = [x]
+myReverse (xs) = myLast(xs) : myReverse(myInit(xs))
