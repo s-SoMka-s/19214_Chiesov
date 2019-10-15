136013924b9f06991d03495076a9c4a2353c0958
s-SoMka-s <S.O.M.K.A@yandex.ru>
2019-10-15 09:14:58 +0700

    12 funcs added

--- lab1-list/lists.hs
+++ lab1-list/lists.hs
@@ -1,3 +1,8 @@
+myGet ::(Eq b, Num b) => [a] -> b -> a
+myGet [] _ = error "Empty list"
+myGet (x:xs) 0 = x
+myGet (x:xs) n = myGet xs (n-1) 
+
 myHead :: [a] -> a
 myHead [] = error "Empty list"
 myHead (x:xs) = x
@@ -22,6 +27,27 @@ myReverse :: [a] -> [a]
 myReverse [] = error "Empty list"
 myReverse [x] = [x]
 myReverse (xs) = myLast(xs) : myReverse(myInit(xs))
+
+myLenght :: Num b => [a] -> b
+myLenght [] = 0
+myLenght (x:xs) = 1 + myLenght xs
+
+myAppend :: [a] -> a -> [a]
+myAppend [] elem = [elem]
+myAppend (x:xs) elem = x : myAppend xs elem
+
+myTake :: (Num b, Eq b) => b -> [a] -> [a]
+myTake _ [] = []
+myTake 0 xs = []
+myTake n (x:xs) = x :( myTake (n-1) xs)
+
+myElem :: Eq a => [a] -> a -> Bool
+myElem [] _ = False
+myElem (x:xs) elem  = if elem == x then True else myElem xs elem
+
+myNull :: [a] -> Bool
+myNull [] = True
+myNull xs = False
 
 myZip :: [a]->[b]->[(a,b)]
 myZip [] [] = error "Empty list"
