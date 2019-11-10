ce0b2f31f99466fe5d0f01a1dbce0a039541723b
s-SoMka-s <32841655+s-SoMka-s@users.noreply.github.com>
2019-11-10 20:04:30 +0700

    Update lists.hs

--- lab1-list/lists.hs
+++ lab1-list/lists.hs
@@ -22,13 +22,12 @@ myInit [] = error "Empty list"
 myInit [x] = []
 myInit (x:xs) = x : myInit xs
 
---make faster - possible O(n) solution
-myReverse :: [a] -> [a]
-myReverse [] = error "Empty list"
-myReverse [x] = [x]
-myReverse xs = myLast(xs) : myReverse(myInit(xs))
+myReverse:: [a] -> [a]
+myReverse xs = helper [] xs
+    where
+        helper acc [] = acc
+        helper acc (x:xs) = helper (x:acc) xs
 
---length not optimal // made faster
 myLenght :: [a] -> Integer
 myLenght [] = 0
 myLenght xs = helper 0 xs where
@@ -39,6 +38,10 @@ myAppend :: [a] -> a -> [a]
 myAppend [] elem = [elem]
 myAppend (x:xs) elem = x : myAppend xs elem
 
+myConcat :: [a] -> [a] -> [a]
+myConcat [] ys = ys
+myConcat (x:xs) ys = x : myConcat xs ys
+
 myDrop :: Integer -> [a] -> [a]
 myDrop 0 xs = xs;
 myDrop n (x:xs) = myDrop (n-1) xs
@@ -48,6 +51,9 @@ myTake _ [] = []
 myTake 0 xs = []
 myTake n (x:xs) = x : ( myTake (n-1) xs)
 
+mySplitAt :: Integer -> [a] -> ([a],[a])
+mySplitAt n xs = (myTake n xs, myDrop n xs)
+
 myElem :: Eq a => [a] -> a -> Bool
 myElem [] _ = False
 myElem (x:xs) elem  = if elem == x then True else myElem xs elem
@@ -56,12 +62,15 @@ myNull :: [a] -> Bool
 myNull [] = True
 myNull xs = False
 
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter _ [] = []
+myFilter cond (x:xs) = if cond x then x : myFilter cond xs else myFilter cond xs
+
 myMap :: (a->b)->[a]->[b]
 myMap _ [] = []
 myMap f (x:xs) = (f x) : myMap f xs 
 
---added checks for empty lists
 myZip :: [a]->[b]->[(a,b)]
 myZip xs [] = []
 myZip [] xy = []
-myZip (x:xs) (y:ys) = (x,y) : myZip xs ys 
+myZip (x:xs) (y:ys) = (x,y) : myZip xs ys
