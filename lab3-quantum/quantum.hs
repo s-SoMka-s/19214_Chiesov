0e0758c9ecfcff491cad4e2c19d7e32f61f67a13
s-SoMka-s <32841655+s-SoMka-s@users.noreply.github.com>
2019-12-19 07:13:51 +0700

    Update quantum.hs

--- lab3-quantum/quantum.hs
+++ lab3-quantum/quantum.hs
@@ -1,4 +1,3 @@
-
 data Complex a = Complex a a
 data QState a = QState a String
 type Qubit a = [QState a]
@@ -26,7 +25,7 @@ instance (Num a, Floating a) => Num (Complex a) where
         (Complex r1 i1) * (Complex r2 i2) = Complex (r1*r2 - i1 * i2) (r1*r2 + i1 * i2)
         abs (Complex r i) = Complex (sqrt $ r * r + i * i) 0
         negate (Complex r i) = Complex r (negate i)
-        fromInteger int  = Complex 0 0
+        fromInteger int  = Complex  (fromInteger int) 0
         signum (Complex r i) = Complex (signum r) 0 
 
 instance Functor QState where
@@ -56,20 +55,23 @@ fromPairList pairList = [(QState complex state) | (complex, state) <- pairList]
 
 scalarProduct:: (Num a, Floating a) => Qubit (Complex a) -> Qubit (Complex a) -> (Complex a)
 scalarProduct [] [] = Complex 0 0
-scalarProduct ((QState c1 _):xs) ((QState c2 _):ys) = (c1 * c2) + (scalarProduct xs ys) 
+--scalarProduct ((QState c1 _):xs) ((QState c2 _):ys) = (c1 * c2) + (scalarProduct xs ys) 
+scalarProduct xs ys = foldl (+) 0 [c1*c2 | ((QState c1 s1), (QState c2 s2)) <- (zip xs ys)]
 
 entagle :: (Num a,  Floating a ) => Qubit (Complex a) -> Qubit (Complex a) -> Qubit (Complex a)
 entagle q1 q2 = [ QState (c1 * c1) (state1 ++ state2) | (QState c1 state1) <- q1 , (QState c2 state2) <- q1]
 
 -------------------------------------------------------------------
 
-c1 = (Complex 3 6)
-c2 = (Complex 5 2)
-c3 = (Complex 1 3)
+c1 = (Complex 3.0 6.0)
+c2 = (Complex 5.0 2.0)
+c3 = (Complex 1.0 3.0)
 
 pairs = [(c1, "ttt"), (c2, "rrr"), (c3, "jjj")]
 
 qs1 = QState c1 "uuu"
 qs2 = QState c2 "ggg"
-qs3 = QState (Complex 8 56) "kkkk"
+qs3 = QState (Complex 8.0 56.0) "kkkk"
 
+
+qb = [qs1, qs2, qs3]
