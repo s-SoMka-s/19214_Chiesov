d9c3f74226f3a69ce3c8bb3ae382199a5f976e0f
s-SoMka-s <32841655+s-SoMka-s@users.noreply.github.com>
2019-12-23 19:13:58 +0700

    size & empty добавлены в реализацию.

--- lab4-hashTable/hashTabel.hs
+++ lab4-hashTable/hashTabel.hs
@@ -1,12 +1,28 @@
 import System.IO
-type Size = Int
-data HashTable key value = HashTable [[(key, value)]] Size deriving (Show)
+import Data.Bits
+import Data.Char
+type MaxSize = Int
+type Capacity = Int
+data HashTable key value = HashTable [[(key, value)]] MaxSize Capacity deriving (Show)
 
 defaultHashTable :: HashTable k v
-defaultHashTable = HashTable [] 0
+defaultHashTable = HashTable [] 0 0
+
+hash :: String -> Int -> Int
+hash key size = (foldl (\acc x -> acc + ord(x)) 0 key) `mod` size
+
+size :: (Show key) => HashTable key value -> Int
+size (HashTable hTable maxSize capacity) = capacity
+
+empty :: (Show key) => HashTable key value -> Bool
+empty (HashTable hTable maxSize capacity)
+                                        | capacity == 0 = True
+                                        | otherwise = False
 
 main :: IO()
 main = do
     content <- lines <$> readFile "result.txt"
     let kvList = [(head $ words x, last $ words x) | x <- content]
     print(kvList)
+
+ht = defaultHashTable
