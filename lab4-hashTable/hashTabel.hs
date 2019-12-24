48934b7fefec6981cd23267c877b602c7b676aaf
s-SoMka-s <32841655+s-SoMka-s@users.noreply.github.com>
2019-12-24 11:55:11 +0700

    добавил erase и clear

--- lab4-hashTable/hashTabel.hs
+++ lab4-hashTable/hashTabel.hs
@@ -6,20 +6,10 @@ type Capacity = Int
 type ECnt = Int
 data HashTable k v = HashTable [[(k, v)]] Capacity ECnt deriving (Show)
 
-
-
 hash :: (Show k) => k -> Int
 hash k = (foldl (\acc x -> acc + ord(x)) 0 (show k))
 
-
-                                                            
-                                                                                        
-
-
-
-
-
-
+                                                                               
 fromList::(Show k, Eq k) => [(k,v)] -> HashTable k v
 fromList list = foldr (\p xs -> insert xs (fst p) (snd p)) (defaultHashTable $ length list * 2 + 1) list
 
@@ -27,6 +17,18 @@ fromList list = foldr (\p xs -> insert xs (fst p) (snd p)) (defaultHashTable $ l
 
 defaultHashTable :: Int -> HashTable k v
 defaultHashTable capacity = HashTable (replicate capacity []) capacity 0
+
+clear :: HashTable k v -> HashTable k v
+clear (HashTable table capacity ecnt) = HashTable (replicate capacity []) capacity 0
+
+erase :: (Show k, Eq k) => HashTable k v -> k -> HashTable k v
+erase (HashTable table capacity ecnt) k = HashTable(
+                                                    map (\(indx, hList) ->  if indx == (hash k) `mod` capacity then
+                                                                                [elem | elem <- hList, fst elem /= k]
+                                                                            else
+                                                                                hList)
+                                                        (zip [0..] table)) capacity (ecnt - 1)
+
 
 insert :: (Show k, Eq k) => HashTable k v -> k -> v -> HashTable k v
 insert (HashTable table capacity ecnt) k v = HashTable(
@@ -50,4 +52,4 @@ main = do
     let kvList = [(head $ words x, last $ words x) | x <- content]
     print(kvList)
 
-ht = defaultHashTable
+ht = defaultHashTable 3
